5fa036f7715c45d0c603263a03c8a21051ce2338
ViscaMB <b_mo@live.nl>
2018-06-04 12:36:00 +0200

    amk

---
+++ resources/views/test.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Scoop</title>
+    <style>
+        table {
+            /* font-family: arial, sans-serif; */
+            width: 100%;
+        }
+        th{
+            border-bottom:1px solid #dddddd;
+        }
+        td, th {
+
+            text-align: left;
+            padding: 8px;
+        }
+    </style>
+</head>
+<body>
+<header>
+
+</header>
+<main>
+
+</main>
+<footer>
+    <table>
+        <tr>
+            <th>Film</th>
+            <th>Genre</th>
+            <th>Mijn film</th>
+            <th>mijn account</th>
+            <th>over Scoop</th>
+            <th>Help</th>
+        </tr>
+        <tr>
+            <td>Top 20</td>
+            <td>Actie</td>
+            <td>gehuurde films</td>
+            <td>instellingen</td>
+            <td>online films</td>
+            <td>veel gestelde vragen</td>
+        </tr>
+        <tr>
+            <td>Alle films</td>
+            <td>Comedy</td>
+            <td>gekochte films</td>
+            <td>tegoed verhogen</td>
+            <td>aanmelden</td>
+            <td>klanten service</td>
+        </tr>
+        <tr>
+            <td>Nieuw</td>
+            <td>Drama</td>
+            <td>Favorieten</td>
+            <td>kadocode inwisselen</td>
+        </tr>
+        <tr>
+            <td>Aanbevelingen</td>
+            <td>Familiefilm</td>
+        </tr>
+        <tr>
+            <td>Top 100</td>
+            <td>Horror</td>
+        </tr>
+    </table>
+</footer>
+
+</body>
+</html>
